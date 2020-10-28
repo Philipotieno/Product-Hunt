be994c40f00140dcc3ac49b052988a26294b2c78
philipotieno <tmitchellb007@gmail.com>
2020-10-28 21:36:28 +0300

    chore: firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDMg1EyaUho4D1pqLQSJHIwvlcQgDDuvbc",
+    authDomain: "philip-hunt.firebaseapp.com",
+    databaseURL: "https://philip-hunt.firebaseio.com",
+    projectId: "philip-hunt",
+    storageBucket: "philip-hunt.appspot.com",
+    messagingSenderId: "398694738371",
+    appId: "1:398694738371:web:ed902a628f3642d6c83576"
+  };
+
+  export default firebaseConfig;
